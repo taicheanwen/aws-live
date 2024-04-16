939ff7464332f103b400a01a29e8f9ecc01f6bcb
taicheanwen <150013501+taicheanwen@users.noreply.github.com>
2024-04-16 15:53:40 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cvm0g8ma6gbq.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "taicheanwen-employee"
+customregion = "us-east-1"
+
